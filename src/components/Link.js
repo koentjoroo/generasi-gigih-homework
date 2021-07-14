af11e18f921526e02bb016e0b3ef763413a3744a
Novan <enerka4ig@gmail.com>
2021-07-14 12:22:51 +0700

    Finished homework `0.2.1`

---
+++ src/components/Link.js
@@ -0,0 +1,14 @@
+import '../App.css'
+
+export default function Link({ to: url, isExternal = false, children }) {
+  return (
+    <a
+      className="App-link"
+      href={url}
+      target={isExternal ? "_blank" : undefined}
+      rel={isExternal ? "noreferrer" : undefined}
+    >
+      {children}
+    </a>
+  );
+}
