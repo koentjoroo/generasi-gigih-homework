afbf5a69ecbbfb606dcfc689bce1bdf011964dbd
Novan <enerka4ig@gmail.com>
2021-07-31 20:30:20 +0700

    Update create playlist page layout

--- src/pages/CreatePlaylist.js
+++ src/pages/CreatePlaylist.js
@@ -1,19 +1,57 @@
-import { useSelector } from 'react-redux'
+import { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { clearSelectedTracks } from '../store/playlist'
+import { FaPlus } from 'react-icons/fa'
 import Track from '../components/Track'
+import Modal from '../components/Modal'
 import PlaylistForm from '../components/PlaylistForm'
+import Button from '../components/Button'
 
 const CreatePlaylist = () => {
-  const { tracks } = useSelector(state => state.playlist)
+  const dispatch = useDispatch()
+  const { tracks, selectedTracks } = useSelector(state => state.playlist)
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
   return (
-    <div style={{ display: 'grid', gridTemplateColumns: '2fr 1fr', gap: '2rem' }}>
-      <div>
+    <div>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
         <h2>Tracks</h2>
+        <span style={{ display: 'flex', alignItems: 'center' }}>
+          <Button
+            onClick={() => dispatch(clearSelectedTracks())}
+            type="button"
+            variant="transparent"
+            disabled={selectedTracks.length === 0}
+          >
+            Clear Selection
+          </Button>
+          <Button
+            icon={<FaPlus />}
+            onClick={() => setIsModalOpen(true)}
+            disabled={selectedTracks.length === 0}
+          >
+            Create Playlist
+          </Button>
+        </span>
+      </div>
+      <div>
         {tracks.map(track => (
           <Track track={track} key={track.id} />
         ))}
       </div>
-      <PlaylistForm />
+      <Modal
+        title="Create Playlist"
+        isOpen={isModalOpen}
+        closeModal={() => setIsModalOpen(false)}
+      >
+        <PlaylistForm />
+      </Modal>
     </div>
   )
 }
